907ab293948b486dace1058b11efb115d4b430a6
ky0yk <ksaik1024@gmail.com>
2021-07-31 11:15:42 +0900

    ルーターを追加

--- src/lambda/router.ts
+++ src/lambda/router.ts
@@ -2,10 +2,37 @@ import express = require('express');
 const router: express.Router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-  res.json({ message: 'Hello World!' });
+  res.json({ message: 'API is working!' });
 });
-router.get('/users', (req: express.Request, res: express.Response) => {
+
+router.get('/bears', (req: express.Request, res: express.Response) => {
+  // TODO 実装
+  console.log('getBears');
   res.json([{ name: 'Taro' }, { name: 'Hanako' }]);
+});
+
+router.post('/bears', (req: express.Request, res: express.Response) => {
+  // TODO 実装
+  console.log('postBears');
+  res.json({ name: 'Taro' });
+});
+
+router.get('/bears/:id', (req: express.Request, res: express.Response) => {
+  // TODO 実装
+  console.log('getBears/id');
+  res.json({ name: req.params.id });
+});
+
+router.put('/bears/:id', (req: express.Request, res: express.Response) => {
+  // TODO 実装
+  console.log('putBears/id');
+  res.json({ name: req.params.id });
+});
+
+router.delete('/bears/:id', (req: express.Request, res: express.Response) => {
+  // TODO 実装
+  console.log('deleteBears/id');
+  res.json({ name: req.params.id });
 });
 
 module.exports = router;
