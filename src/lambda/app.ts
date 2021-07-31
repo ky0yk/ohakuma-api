ae525655b3d6295f4decea998ef799323e51039c
ky0yk <40429527+ky0yk@users.noreply.github.com>
2021-07-31 10:50:29 +0900

    Revert "serverless-expressのセットアップ"

--- src/lambda/app.ts
+++
@@ -1,8 +0,0 @@
-import express = require('express');
-const app: express.Express = express();
-
-const router: express.Router = require('./router');
-
-app.use('/', router);
-
-module.exports = app;
