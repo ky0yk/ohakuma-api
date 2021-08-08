31a5fa86c363e395d6eebfcbd7d0ae0c160a0bfb
ky0yk <ksaik1024@gmail.com>
2021-08-09 07:57:07 +0900

    モックへの入力値を確認するように変更

--- test/lambda/dynamodb-bear-management-table.test.ts
+++ test/lambda/dynamodb-bear-management-table.test.ts
@@ -4,6 +4,8 @@ import { GetCommand, ScanCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { Bear } from '../../src/lambda/domains/bear-management/bear-management';
 import { v4 as uuidv4 } from 'uuid';
 
+const tableName = process.env.TABLE_NAME;
+
 const ddbMock = mockClient(infra.ddbDocClient);
 
 it('全クマ情報の取得ができること', async () => {
@@ -11,9 +13,13 @@ it('全クマ情報の取得ができること', async () => {
     { id: uuidv4(), name: 'ヒグマ', info: 'ヒトはヒグマに勝てねえ' },
     { id: uuidv4(), name: 'シロクマ', info: '白いクマです' },
   ];
-  ddbMock.on(ScanCommand).resolves({
-    Items: items,
-  });
+  ddbMock
+    .on(ScanCommand, {
+      TableName: tableName,
+    })
+    .resolves({
+      Items: items,
+    });
   const response = await infra.getAllBears();
   expect(response).toStrictEqual(items);
 });
@@ -21,10 +27,14 @@ it('全クマ情報の取得ができること', async () => {
 it('IDに対応するクマ情報の取得ができること', async () => {
   const uuid = uuidv4();
   const item = { id: uuid, name: 'ヒグマ', info: 'ヒトはヒグマに勝てねえ' };
-  ddbMock.on(GetCommand).resolves({
-    Item: item,
-  });
-
+  ddbMock
+    .on(GetCommand, {
+      TableName: tableName,
+      Key: { id: uuid },
+    })
+    .resolves({
+      Item: item,
+    });
   const response = await infra.getBear(uuid);
   console.log(response);
   expect(response).toStrictEqual(item);
@@ -32,10 +42,14 @@ it('IDに対応するクマ情報の取得ができること', async () => {
 
 it('クマ情報の作成ができること', async () => {
   const item = { id: uuidv4(), name: 'ヒグマ', info: 'ヒトはヒグマに勝てねえ' };
-  ddbMock.on(PutCommand).resolves({
-    Attributes: item,
-  });
-
+  ddbMock
+    .on(PutCommand, {
+      TableName: tableName,
+      Item: item,
+    })
+    .resolves({
+      Attributes: item,
+    });
   const response = await infra.createBear(item);
   console.log(response);
   expect(response).toStrictEqual(item);
