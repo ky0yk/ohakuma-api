8c3ce2cf952b30cf9dafd56d893f7f7089248e51
ky0yk <ksaik1024@gmail.com>
2021-08-14 13:36:25 +0900

    Fine-grained assertions での最小限のテストを追加

--- test/ohakuma-api.test.ts
+++ test/ohakuma-api.test.ts
@@ -1,13 +1,27 @@
-// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-// import * as cdk from '@aws-cdk/core';
-// import * as OhakumaApi from '../lib/ohakuma-api-stack';
+import {
+  expect as expectCDK,
+  countResources,
+  haveResource,
+} from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as OhakumaApi from '../lib/ohakuma-api-stack';
 
-// test('Empty Stack', () => {
-//     const app = new cdk.App();
-//     // WHEN
-//     const stack = new OhakumaApi.OhakumaApiStack(app, 'MyTestStack');
-//     // THEN
-//     expectCDK(stack).to(matchTemplate({
-//       "Resources": {}
-//     }, MatchStyle.EXACT))
-// });
+const app = new cdk.App();
+const stack = new OhakumaApi.OhakumaApiStack(app, 'OhakumaApiStack');
+
+test('Lambda', () => {
+  expectCDK(stack).to(countResources('AWS::Lambda::Function', 1));
+});
+
+test('API Gateway', () => {
+  expectCDK(stack).to(countResources('AWS::ApiGateway::RestApi', 1));
+});
+
+test('DynamoDB', () => {
+  expectCDK(stack).to(countResources('AWS::DynamoDB::Table', 1));
+  expectCDK(stack).to(
+    haveResource('AWS::DynamoDB::Table', {
+      TableName: 'ohakumaTable',
+    })
+  );
+});
